35a5f44a460bd67b9c54fd7e5bd22dcff0d1991a
weizel <weizel@users.noreply.github.com>
2013-11-21 14:17:08 +0100

    Knockout structure improvements.

--- knockout/app/main.js
+++ knockout/app/main.js
@@ -1,3 +1,40 @@
 // to depend on a bower installed component:
 // define(['component/componentName/file'])
 
+require(['jquery', 'ko'], function($, ko) {
+	$(function() {
+		//knockout application
+
+		/* ************ ViewModel ************** */
+		var AppModel = function(users) {
+			var self = this;
+			self.title = ko.observable('Initial Title');
+			self.currentUserName = ko.observable();
+			self.currentUserEmail = ko.observable();
+			self.users = ko.observableArray(ko.utils.arrayMap(users, function(user) {
+				return new User(user.title, user.completed);
+			}));
+			self.createUser = function() {
+				self.users.push(new User(self.currentUserName(), self.currentUserEmail()));
+			};
+			self.deleteUser = function() {
+				self.users.remove(this)
+			};
+		};
+
+		var User = function(name, email, editing) {
+			var self = this;
+			self.name = ko.observable(name);
+			self.email = ko.observable(email);
+			self.editing = ko.observable(false);
+			self.edit = function(){
+				self.editing(!self.editing());
+				console.log(this);
+			};
+		};
+
+		/* *********** run App ************* */
+		var users = [];
+		ko.applyBindings(new AppModel(users));
+	});
+})
